f62a4434512dbc0ee2b450bb03730bba827abee8
gguerreroi <gguerrero@sonivitac.com>
2021-08-01 16:04:20 -0600

    Creacion de Estructura de Proyecto

--- src/middlewares/passport.js
+++ src/middlewares/passport.js
@@ -11,7 +11,7 @@ passport.use('local.signin', new LocalStrategy({
     passReqToCallback: true
 }, async (request, username, password, done) => {
 
-    const URL = `${app.locals.UrlApi}/v1/Login`
+    const URL = `${app.locals.UrlApi}/asociados/login`
 
     await axios.get(URL,{
         auth: {
@@ -21,26 +21,11 @@ passport.use('local.signin', new LocalStrategy({
     }).then(function(response){
         const {state, data} = response.data;
         const {Code, Message} = state;
-        const {CodEmpleado, NombreCompleto, Usuario, Token, Rol, CodRegion, CodSubRegion} = data;
-        console.log(Code, Message);
-        request.session.CodEmpleado = CodEmpleado;
-        request.session.NombreCompleto = NombreCompleto;
-        request.session.UserAuth = Usuario;
-        request.session.TokenAuth = Token;
-        request.session.Rol = Rol;
-        request.session.CodRegion = CodRegion;
-        request.session.CodSubRegion = CodSubRegion;
-        const User = {
-            TokenAuth: Token,
-            UserAuth: Usuario,
-            CodEmpleado: CodEmpleado,
-            NombreCompleto: NombreCompleto,
-            Rol: Rol,
-            CodRegion: CodRegion,
-            CodSubRegion: CodSubRegion
-        }
 
-        return done(null, User, request.flash('success', Token))
+        console.log(Code, Message);
+        request.session.InfoUser = data;
+
+        return done(null, data, request.flash('success', data.Token))
     }).catch(function(error){
         console.log("error", error)
         return done(null, null, request.flash('message', 'El usuario o contraseña no es Correcto'));
@@ -55,27 +40,27 @@ passport.serializeUser(function(user, done){
 passport.deserializeUser(async function(request, User, done){
     // console.log("deserializeUser", User)
 
-    const {CodEmpleado, TokenAuth} = User;
-
-    const URL = `http://api-poad.inabregion2.org/v1/Usuarios/${CodEmpleado}`
-    axios.get(`${URL}`,{
-        headers: {
-            'x-access-token': `${TokenAuth}`
-        }
-    }).then(function(response){
-        let Usuario
-
-        if (response.status == 200){
-            Usuario = response.data.data;
-            request.session.NombreCompleto = Usuario.NombreCompleto;
-            request.session.CodRegion = Usuario.CodRegion;
-            request.session.CodSubRegion = Usuario.CodSubRegion;
-            request.session.Rol = Usuario.Rol;
-        }
-        done(null, User);
-    }).catch(function (error){
-        console.log("error", error)
-        done(null, User);
-    })
+    // const {CodCliente, Token} = User;
+    done(null, User);
+    // const URL = `http://api-poad.inabregion2.org/asociados/${CodEmpleado}`
+    // axios.get(`${URL}`,{
+    //     headers: {
+    //         'x-access-token': `${TokenAuth}`
+    //     }
+    // }).then(function(response){
+    //     let Usuario
+    //
+    //     if (response.status == 200){
+    //         Usuario = response.data.data;
+    //         request.session.NombreCompleto = Usuario.NombreCompleto;
+    //         request.session.CodRegion = Usuario.CodRegion;
+    //         request.session.CodSubRegion = Usuario.CodSubRegion;
+    //         request.session.Rol = Usuario.Rol;
+    //     }
+    //     done(null, User);
+    // }).catch(function (error){
+    //     console.log("error", error)
+    //     done(null, User);
+    // })
 
 });
