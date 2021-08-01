4690004739a951eed4b52a2ec6c5103e619d3f1c
gguerreroi <gguerrero@sonivitac.com>
2021-08-01 15:47:35 -0600

    Creacion de Estructura de Proyecto

---
+++ src/controllers/login.controller.js
@@ -0,0 +1,9 @@
+const passport = require('passport');
+
+export const checkLogin = (request, response, next) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/',
+        failureRedirect: '/Auth',
+        failureFlash: true
+    })(request, response, next);
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,14 @@
 import {Router} from 'express';
 import * as controller from '../controllers/controller'
 import * as check from "../middlewares/islogged";
-
+import * as login from '../controllers/login.controller'
 
 const r = Router();
 
-r.get('/',
-    check.isAuth,
-    controller.MainAsociados);
+r.get('/', check.isAuth, controller.MainAsociados);
 
-r.get(
-    "/Auth",
-    check.isAuthLogin,
-    controller.AuthView);
+r.get("/Auth", check.isAuthLogin, controller.AuthView);
 
+r.post('/Auth', [], login.checkLogin)
 
 export default r
